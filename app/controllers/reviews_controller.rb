930af881e336978db74704a73baf354189f4a4b9
bryant tee <bryantt23@gmail.com>
2015-03-01 01:37:39 -0800

    has ratings, but no stars yet

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,7 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :set_movie
   before_action :authenticate_user!
-  
-  def index
-    @reviews = Review.all
-  end
-
-  def show
-  end
 
   def new
     @review = Review.new
@@ -19,9 +13,11 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.user_id = current_user.id
+    @review.movie_id = @movie.id
+
 
     if @review.save
-      redirect_to @review
+      redirect_to @movie
     else
       render :new
     end
@@ -43,6 +39,10 @@ class ReviewsController < ApplicationController
     @review = Review.find(params[:id])
   end
 
+  def set_movie
+    @movie = Movie.find(params[:movie_id])
+  end
+  
   # Never trust parameters from the scary internet, only allow the white list through.
   def review_params
     params.require(:review).permit(:rating, :comment)
